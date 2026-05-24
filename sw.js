f6827711fb45ce43244dc661ee5c8cca6236c391
oliver-red <bakhyeonsu@gmail.com>
2026-05-24 09:25:26 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,18 @@
-const CACHE = 'coffee-recipe-v3';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE = 'coffee-recipe-v5';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
 });
 
 self.addEventListener('fetch', e => {
-  // API 요청은 항상 네트워크 사용
   if (e.request.url.includes('script.google.com')) return;
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
